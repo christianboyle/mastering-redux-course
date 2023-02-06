02d0d845f185d4b79b02ce48d70cc2d9bd31fff6
Christian Boyle <mail@christianboyle.com>
2023-02-06 10:55:13 -0600

    add basics > array filter

---
+++ src/basics/array-filter.js
@@ -0,0 +1,12 @@
+const employees = [
+  { name: 'David Carlson', age: 30 },
+  { name: 'John Cena', age: 34 },
+  { name: 'Mike Sheridan', age: 25 },
+  { name: 'John Carte', age: 50 }
+]
+
+const employee = employees.filter(function (employee) {
+  return employee.name.indexOf('John') > -1
+})
+
+console.log(employee) // [ { name: 'John Cena', age: 34 }, { name: 'John Carte', age: 50 } ]
