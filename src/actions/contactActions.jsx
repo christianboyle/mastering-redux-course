9bc141615ba24033dec4e2a2ec75d5575e2a9dac
Christian Boyle <mail@christianboyle.com>
2023-03-10 09:13:10 -0600

    use constants for action types

--- src/actions/contactActions.jsx
+++ src/actions/contactActions.jsx
@@ -1,13 +1,15 @@
+import { ADD_CONTACT, DELETE_CONTACT } from '../utils/constants'
+
 export const addContact = (contact) => {
   return {
-    type: 'ADD_CONTACT',
+    type: ADD_CONTACT,
     contact
   }
 }
 
 export const deleteContact = (id) => {
   return {
-    type: 'DELETE_CONTACT',
+    type: DELETE_CONTACT,
     id
   }
 }
--- src/reducers/contactReducer.jsx
+++ src/reducers/contactReducer.jsx
@@ -1,8 +1,10 @@
+import { ADD_CONTACT, DELETE_CONTACT } from '../utils/constants'
+
 const contactReducer = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_CONTACT':
+    case ADD_CONTACT:
       return [...state, action.contact]
-    case 'DELETE_CONTACT':
+    case DELETE_CONTACT:
       return state.filter((contact) => contact.id !== action.id)
     default:
       return state
