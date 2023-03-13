cd4dbeed79319c1b5e2f91088aadc5c277790fda
Christian Boyle <mail@christianboyle.com>
2023-03-13 12:27:21 -0500

    init products listing app

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return <h1 className='header'>Random Users Generator</h1>
-}
-
-export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store/store'
 import App from './App'
 import './index.css'
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'))
-rootElement.render(
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>,
 )
