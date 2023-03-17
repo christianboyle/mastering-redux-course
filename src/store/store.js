cedf5a0243248a87733f131bb539e823bd73b0a9
Christian Boyle <mail@christianboyle.com>
2023-03-17 14:07:25 -0500

    update redux devtools config

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,16 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
 import categoriesReducer from '../reducers/categoriesReducer'
 import productsReducer from '../reducers/productsReducer'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   combineReducers({
     categories: categoriesReducer,
     products: productsReducer
   }),
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 )
 
 store.subscribe(() => {
